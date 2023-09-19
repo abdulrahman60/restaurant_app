55640a969282a477afadd6a9e1a09a311a5b8f35
abdulrahman mohamud <abdulrahman23mahamud@gmail.com>
2023-09-19 12:47:48 +0400

    version 1

---
+++ lib/categories_screen.dart
@@ -0,0 +1,23 @@
+import 'dart:js_interop';
+
+import 'package:flutter/material.dart';
+import './models/category.dart';
+import './dummy_data.dart';
+import './category_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+      children: [DUMMY_CATEGORIES]
+          .map((catData) => CategoryItem(catData.title))
+          .toList(),
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
+      ),
+    );
+  }
+}
---
+++ lib/dummy_data.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import './models/category.dart';
+
+const DUMMY_CATEGORIES = const [
+  Category('c1', 'Italian'),
+  Category('c2', 'Pizza Hut'),
+];
